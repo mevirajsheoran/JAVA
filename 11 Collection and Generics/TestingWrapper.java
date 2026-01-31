dc9fd889da2bf3154249a5a33c22f49c49c5b259
Viraj Sheoran <viraj.sheoran10@gmail.com>
2026-01-31 20:52:05 +0530

    11

---
+++ 11 Collection and Generics/TestingWrapper.java
@@ -0,0 +1,10 @@
+package in.encapsulation.wrapper;
+
+public class TestingWrapper {
+    public static void main(String[] args) {
+        Integer first = 55;
+        System.out.println(first);
+        int second = first;
+
+    }
+}
