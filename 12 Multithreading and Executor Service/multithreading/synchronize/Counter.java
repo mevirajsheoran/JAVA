bd3142669fd2f1ebc02f5afd7a83a4c2d86f8b28
Viraj Sheoran <viraj.sheoran10@gmail.com>
2026-02-07 14:54:02 +0530

    12

---
+++ 12 Multithreading and Executor Service/multithreading/synchronize/Counter.java
@@ -0,0 +1,14 @@
+package in.encapsulation.multithreading.synchronize;
+
+public class Counter {
+
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
